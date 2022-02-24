a4b8c9c784d5e55b31245bec1f7aa12e0a6e3ce4
almadef <kaberton@outlook.com>
2022-02-24 21:24:13 +0400

    GH37.SEO.исправил работу в админке с кэшем

--- backend/controllers/CategoryController.php
+++ backend/controllers/CategoryController.php
@@ -59,14 +59,6 @@ final class CategoryController extends Controller implements MergeBaseActionInte
     }
 
     /**
-     * @return array
-     */
-    public function getCacheTags()
-    {
-        return ['library_index'];
-    }
-
-    /**
      * @param $id
      * @return Category|null
      * @throws NotFoundHttpException
